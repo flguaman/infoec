a226d522dc99f662f0135be84707dcf7f207b79f
FREDDY LEONARDO GUAMAN LOJA <flguaman@sudamericano.edu.ec>
2025-11-03 19:24:10 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "studio-6270471985-893fe",
+  "appId": "1:84753780470:web:63e654bb446338c6b766d7",
+  "apiKey": "AIzaSyA-vHm0a_hpmzQYKq13u-37fdTFD4s-MV4",
+  "authDomain": "studio-6270471985-893fe.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "84753780470",
+  "storageBucket": "studio-6270471985-893fe.appspot.com"
 };
 
 // Initialize Firebase
