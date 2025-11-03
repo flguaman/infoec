321dd148fc1fffd40454d771221bd531beeb407c
FREDDY LEONARDO GUAMAN LOJA <flguaman@sudamericano.edu.ec>
2025-11-03 19:27:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6270471985-893fe",
+  "appId": "1:84753780470:web:63e654bb446338c6b766d7",
+  "apiKey": "AIzaSyB-iZpbYMkCJGw7A5TgfIyAMJ3N9dLKBwU",
+  "authDomain": "studio-6270471985-893fe.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "84753780470"
+};
